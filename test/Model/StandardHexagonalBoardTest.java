f8404df3f2a3b54cb71afb68fa18a74060ac860c
A. Sharma <ankagugu@gmail.com>
2023-10-28 19:25:00 -0400

    board.occupyTile() done

--- test/Model/StandardHexagonalBoardTest.java
+++ test/Model/StandardHexagonalBoardTest.java
@@ -7,22 +7,12 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-import Model.Hexagon;
-import Model.StandardHexagonalBoard;
-
 public class StandardHexagonalBoardTest {
-  StandardHexagonalBoard board;
-  int boardSize;
-
-  @Before
-  public void init() {
-    this.boardSize = 1;
-    this.board = new StandardHexagonalBoard(1);
-  }
-
   @Test
   public void testConstructorGeneratesHexagonsCorrectly() {
-    Assert.assertEquals(this.board.getBoard(), new ArrayList<>(
+    StandardHexagonalBoard standardBoard = new StandardHexagonalBoard(1);
+
+    Assert.assertEquals(standardBoard.getBoard(), new ArrayList<>(
             Arrays.asList(new Hexagon(0,0,0), new Hexagon(-1, 0, +1),
                     new Hexagon(0, -1, +1),
                     new Hexagon(+1, -1, 0), new Hexagon(+1, 0, -1),
@@ -32,14 +22,14 @@ public class StandardHexagonalBoardTest {
 
   @Test
   public void testConstructorGeneratesMultipleRingHexagons() {
-    this.board = new StandardHexagonalBoard(3);
-    Assert.assertEquals(this.board.getBoard().size(), 37);
+    StandardHexagonalBoard standardBoard = new StandardHexagonalBoard(3);
+    Assert.assertEquals(standardBoard.getBoard().size(), 37);
   }
 
   @Test
   public void testConstructorGeneratesMultipleRingsII() {
-    this.board = new StandardHexagonalBoard(4);
-    Assert.assertEquals(this.board.getBoard().size(), 61);
+    StandardHexagonalBoard standardBoard = new StandardHexagonalBoard(4);
+    Assert.assertEquals(standardBoard.getBoard().size(), 61);
   }
 
   /*@Test
