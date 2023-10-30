c44d3b9a660720822613161ac70e121bbb94fd24
A. Sharma <ankagugu@gmail.com>
2023-10-29 22:07:47 -0400

    working on getting string representation of board, have 2d array of board done

---
+++ src/View/AbstractHexagonRepresentation.java
@@ -0,0 +1,66 @@
+package View;
+
+import java.sql.Array;
+import java.util.Map;
+
+import Model.Color;
+import Model.Hexagon;
+import Model.PlayingBoard;
+
+/**
+ * Represents the board as a 2-d array.
+ */
+public abstract class AbstractHexagonRepresentation {
+
+  /**
+   * Represents the flat-top hexagon board as a 2-D array of integers in the shape of the board.
+   * The first row will be the same as the board size plus 1, while the middle row will be the same
+   * length as the widest part of the hexagon board.
+   * The integers will represent the colors: 0 means not occupied, 1 is the first color from
+   * the enum class and 2 is the second color.
+   *
+   * @param boardState The state of the current board.
+   * @return A 2-D jagged array of integers
+   */
+  public static int[][] boardByNumber(PlayingBoard boardState) {
+    int boardSize = boardState.getSize();
+    int maxWidth = boardSize * 2 + 1;
+
+    //create a list of zeros with the proper size
+    int[][] board = new int[maxWidth][];
+    int rowIndex = 0;
+    //top half of the hexagon board, including the middle row
+    for (int rowSize = boardSize + 1; rowSize <= maxWidth; rowSize++) {
+      board[rowIndex] = new int[rowSize];
+      rowIndex++;
+    }
+    //bottom half of the hexagon board, excluding the middle row
+    for (int rowSize = maxWidth - 1; rowSize >= boardSize + 1; rowSize--) {
+      board[rowIndex] = new int[rowSize];
+      rowIndex++;
+    }
+
+    //go through the occupied list
+    for (Map.Entry<Hexagon, Color> disc : boardState.getOccupiedTiles().entrySet()) {
+      Hexagon tile = disc.getKey();
+      Color color = disc.getValue();
+
+      //get row (up and down)
+      int row = tile.getR() + boardSize;
+
+      //get col (left and right)
+      int col;
+      int q = tile.getQ();
+      if (q >= boardSize) {
+        col = q + boardSize;
+      } else {
+        col = q + row;
+      }
+
+      //change the zero if occupied
+      board[row][col] = color.ordinal() + 1;
+    }
+
+    return board;
+  }
+}
